7387cb1efbd44231e530aa4dd0d352f78f2dd44e
Sami A.Delgado <samiadelgado@gmail.com>
2016-09-27 19:30:32 +0200

    Add files via upload

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,7 +3,12 @@
    'use strict';
    
    $(document).ready(function(){
-       
-   });
+	  $('.slider1').bxSlider({
+	    slideWidth: 200,
+	    minSlides: 2,
+	    maxSlides: 3,
+	    slideMargin: 10
+	  });
+	});
    
 })(jQuery);
